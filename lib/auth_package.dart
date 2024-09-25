f01b12ccd784feb07e7159655dc3e3c98b4a23f6
KarimRaouf <karimzraouf@gmail.com>
2024-09-25 18:38:34 +0300

    auth

---
+++ lib/auth_package.dart
@@ -0,0 +1,31 @@
+library auth_package;
+
+// lib/auth_service.dart
+
+import 'package:flutter/material.dart';
+
+class AuthService {
+  // Example method for signing up a user
+  Future<void> signUp(String email, String password) async {
+    // Your sign-up logic here (e.g., Firebase, REST API)
+    print('User signed up with email: $email');
+  }
+
+  // Example method for logging in a user
+  Future<void> login(String email, String password) async {
+    // Your login logic here (e.g., Firebase, REST API)
+    print('User logged in with email: $email');
+  }
+
+  // Example method for logging out a user
+  Future<void> logout() async {
+    // Your logout logic here (e.g., Firebase, REST API)
+    print('User logged out');
+  }
+
+  // Example method for password reset
+  Future<void> resetPassword(String email) async {
+    // Your password reset logic here (e.g., Firebase, REST API)
+    print('Password reset email sent to: $email');
+  }
+}
---
+++ test/auth_package_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:auth_package/auth_package.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = AuthService();
+  });
+}
